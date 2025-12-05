2ba8e2786cf2c2a98f5403070f1b25fdc58824c6
edwincastilloCNMH <edwin.castillo@cnmh.gov.co>
2025-12-04 23:22:17 -0500

    Se finaliza desarrollo integración con microservicios.

---
+++ WebAPI.Domain/Entities/DocumentoEntity.cs
@@ -0,0 +1,49 @@
+﻿namespace WebAPI.Domain.Entities
+{
+    public class DocumentoEntity
+    {
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string TituloAtribuido { get; set; }
+
+        public int? FechaCreacionI1 { get; set; }
+        public int? FechaCreacionI2 { get; set; }
+        public int? FechaCreacionI3 { get; set; }
+
+        public int? FechaCreacionF1 { get; set; }
+        public int? FechaCreacionF2 { get; set; }
+        public int? FechaCreacionF3 { get; set; }
+
+        public string ObservacionesFechas { get; set; }
+        public int? NivelDescripcion { get; set; }
+
+        public string CantidadVolumenSop { get; set; }
+        public string VolumenSoporte { get; set; }
+        public int? Soporte { get; set; }
+
+        public string NombreProductores { get; set; }
+        public string AlcanceContenido { get; set; }
+        public string TipoDocumental { get; set; }
+
+        public string Personal { get; set; }
+        public string Corporativos { get; set; }
+        public string Tematicos { get; set; }
+        public string Geograficos { get; set; }
+        public string Cronologicos { get; set; }
+
+        public string CondicionAcceso { get; set; }
+        public string CondicionReproducci { get; set; }
+        public string Idioma { get; set; }
+
+        public string NotasContenido { get; set; }
+        public string NotasGenerales { get; set; }
+        public string ReglasNormas { get; set; }
+
+        public DateTime? FechasDescripcion { get; set; }
+        public string EstadoElaboracion { get; set; }
+        public string IdiomaDescripcion { get; set; }
+
+        public string Ruta { get; set; }
+        public string DigitalFiles { get; set; }
+    }
+}
--- WebAPI.Infrastructure/Repositories/ConsultaRepository.cs
+++ WebAPI.Infrastructure/Repositories/ConsultaRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Azure.Core;
 using Newtonsoft.Json;
 using System.Text;
 using WebAPI.Domain.Entities;
@@ -57,6 +58,88 @@ namespace WebAPI.Infrastructure.Repositories
             catch (Exception ex)
             {
                 Console.WriteLine($"[Repository Error] ConsultaRepository - GetBasicQuery: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<List<ResponseEntity>> GetAdvancedQuery(string repo, QueryRequestEntity request)
+        {
+            var response = new List<ResponseEntity>();
+            try
+            {
+                using (var context = new WebAPIContext())
+                {
+                    var model = context.SourceConfig.FirstOrDefault(x => x.Name == repo);
+
+                    if (model == null)
+                        throw new Exception("No hay una configuración para ese repositorio en la tabla SourceConfig.");
+
+
+                    var basicQueryEndpoint = "Consulta/ConsultaAvanzada";
+                    var uri = model.BaseUrl + basicQueryEndpoint;
+
+                    // Realizar llamado POST
+                    using (var httpClient = new HttpClient())
+                    {
+                        string jsonRequest = JsonConvert.SerializeObject(request);
+                        HttpContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                        var result = await httpClient.PostAsync(uri, content);
+                        result.EnsureSuccessStatusCode();
+
+                        var jsonResponse = await result.Content.ReadAsStringAsync();
+
+                        var apiResponse = JsonConvert.DeserializeObject<ResponseModel<ConsultaResponseEntity>>(jsonResponse);
+
+                        if (apiResponse != null && apiResponse.Succeeded && apiResponse.Data != null)
+                            response = apiResponse.Data.Respuesta;
+
+                        return response;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Repository Error] ConsultaRepository - GetAdvancedQuery: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<DocumentoEntity> GetDocumentByCode(string code)
+        {
+            var response = new DocumentoEntity();
+            try
+            {
+                using (var context = new WebAPIContext())
+                {
+                    var model = context.SourceConfig.FirstOrDefault(x => x.Name == "SAIA");
+
+                    if (model == null)
+                        throw new Exception("No hay una configuración para ese repositorio en la tabla SourceConfig.");
+
+
+                    var basicQueryEndpoint = "Consulta/ConsultaAvanzada/";
+                    var uri = $"{model.BaseUrl}{basicQueryEndpoint}{code}";
+
+                    using (var httpClient = new HttpClient())
+                    {
+                        var result = await httpClient.GetAsync(uri);
+                        result.EnsureSuccessStatusCode();
+
+                        var jsonResponse = await result.Content.ReadAsStringAsync();
+
+                        var apiResponse = JsonConvert.DeserializeObject<ResponseModel<DocumentoEntity>>(jsonResponse);
+
+                        if (apiResponse != null && apiResponse.Succeeded && apiResponse.Data != null)
+                            response = apiResponse.Data;
+
+                        return response;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Repository Error] ConsultaRepository - GetDocumentByCode: {ex.Message}");
                 throw;
             }
         }
